c16077a1fed108429926360b4811ae62e113bc98
woldier <1098582358@qq.com>
2023-06-14 14:33:44 +0800

    哨兵模式(sentinel)单链表实现

---
+++ src/main/java/com/woldier/datastruacture/ch2/d01_linked_list/SingleLinkedListSentinel.java
@@ -0,0 +1,10 @@
+package com.woldier.datastruacture.ch2.d01_linked_list;
+
+/**
+ * @author woldier
+ * @version 1.0
+ * @description TODO
+ * @date 2023/6/14 14:32
+ **/
+public class SingleLinkedListSentinel {
+}
