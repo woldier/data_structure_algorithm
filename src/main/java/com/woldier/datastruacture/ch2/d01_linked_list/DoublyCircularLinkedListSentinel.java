fc69cf800f58e8545902781711228053e7825fe9
woldier <1098582358@qq.com>
2023-06-16 12:59:00 +0800

    带哨兵的双向环形链表,实现了remove方法

--- src/main/java/com/woldier/datastruacture/ch2/d01_linked_list/DoublyCircularLinkedListSentinel.java
+++ src/main/java/com/woldier/datastruacture/ch2/d01_linked_list/DoublyCircularLinkedListSentinel.java
@@ -8,53 +8,51 @@ import java.util.Iterator;
  * @description 带哨兵的双向环形链表
  * @date 2023/6/16 10:42
  **/
-public class DoublyCircularLinkedListSentinel implements Iterable<Integer>{
+public class DoublyCircularLinkedListSentinel implements Iterable<Integer> {
 
     Node head;
 
     /**
-    *
-    * description 迭代器对象
-    *
-    *
-    * @return java.util.Iterator<java.lang.Integer>
-    * @author: woldier
-    * @date: 2023/6/16 10:53
-    */
+     * description 迭代器对象
+     *
+     * @return java.util.Iterator<java.lang.Integer>
+     * @author: woldier
+     * @date: 2023/6/16 10:53
+     */
     @Override
     public Iterator<Integer> iterator() {
         return new Iterator<Integer>() {
             Node p = head.next;
+
             @Override
             public boolean hasNext() {
-                return p!=head;
+                return p != head;
             }
 
             @Override
             public Integer next() {
-                int value  = p.value;
+                int value = p.value;
                 p = p.next;
                 return value;
             }
         };
     }
 
-    private static class Node{
+    private static class Node {
         Node prev;
         int value;
         Node next;
 
         /**
-        *
-        * description 构造器
-        *
-        * @param prev 前驱节点
+         * description 构造器
+         *
+         * @param prev  前驱节点
          * @param value 保存的值
          * @param next  后继节点
-        * @return
-        * @author: woldier
-        * @date: 2023/6/16 10:43
-        */
+         * @return
+         * @author: woldier
+         * @date: 2023/6/16 10:43
+         */
         public Node(Node prev, int value, Node next) {
             this.prev = prev;
             this.value = value;
@@ -63,25 +61,115 @@ public class DoublyCircularLinkedListSentinel implements Iterable<Integer>{
     }
 
     /**
-    *
-    * description 构造方法初始化哨兵节点
-    *
-    *
-    * @return
-    * @author: woldier
-    * @date: 2023/6/16 10:45
-    */
+     * description 构造方法初始化哨兵节点
+     *
+     * @return
+     * @author: woldier
+     * @date: 2023/6/16 10:45
+     */
     public DoublyCircularLinkedListSentinel() {
-        head = new Node(null,Integer.MIN_VALUE,null);
+        head = new Node(null, Integer.MIN_VALUE, null);
         head.next = head;
         head.prev = head;
     }
 
-    public void addFirst(int value){
+    /**
+     * description 头部添加
+     *
+     * @param value 值
+     * @return void
+     * @author: woldier
+     * @date: 2023/6/16 11:05
+     */
+
+    public void addFirst(int value) {
         head.value = value;//将当前哨兵变为普通节点
-        head = new Node(head.prev,Integer.MIN_VALUE,head); //创建新的哨兵
+        head = new Node(head.prev, Integer.MIN_VALUE, head); //创建新的哨兵
         head.next.prev = head;
         head.prev.next = head;
+    }
+
+    /**
+     * description 尾部添加
+     *
+     * @param value 值
+     * @return void
+     * @author: woldier
+     * @date: 2023/6/16 11:05
+     */
+    public void addLast(int value) {
+        Node tail = head.prev;
+        tail = new Node(tail, value, tail.next);
+        tail.prev.next = tail;
+        tail.next.prev = tail;
+    }
+
+    /**
+     * description 删除链表头部元素,这里需要判断
+     *
+     * @return void
+     * @author: woldier
+     * @date: 2023/6/16 11:21
+     */
+    public void removeFirst() {
+        if (head.next == head)
+            throw illegal();
+        Node remove = head;
+
+        //head 指向新的哨兵
+        head = head.next;
+        head.value = Integer.MIN_VALUE;
+        //成环
+        head.prev = remove.prev;
+        head.prev.next = head;
+        //help GC
+        remove.prev = null;
+        remove.next = null;
+    }
+
+    /**
+     * description 删除队尾元素
+     *
+     * @return void
+     * @author: woldier
+     * @date: 2023/6/16 11:31
+     */
+    public void removeLast() {
+        if (head.next == head)
+            throw illegal();
+        Node remove = head.prev;
+//        remove.prev.next = head;
+//        head.prev = remove.prev;
+//        //help GC
+//        remove.prev = null;
+//        remove.next = null;
+        removeNode(remove);
+    }
+
+    public void removeByValue(int value) {
+        Node remove = findByValue(value);
+        if (remove == null)
+            throw new IllegalArgumentException("查找失败");
+        removeNode(remove);
+
+    }
+
+    private static void removeNode(Node remove) {
+        remove.prev.next = remove.next;
+        remove.next.prev = remove.prev;
+        remove.prev = remove.next = null;
+    }
+
+    public Node findByValue(int value) {
+        Node p = head.next;
+        while (p != head && p.value != value)
+            p = p.next;
+        return p != head ? p : null;
+
+    }
+
+    private static IllegalArgumentException illegal() {
+        return new IllegalArgumentException("当前链表尾空,无法进行remove操作");
     }
 
 }
---
+++ src/main/java/com/woldier/datastruacture/ch2/d01_linked_list/test/Test4DoublyCircularLinkedList.java
@@ -0,0 +1,45 @@
+package com.woldier.datastruacture.ch2.d01_linked_list.test;
+
+import com.woldier.datastruacture.ch2.d01_linked_list.DoublyCircularLinkedListSentinel;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+import java.util.Arrays;
+
+/**
+ * @author woldier
+ * @version 1.0
+ * @description 带哨兵的双向环形链表测试类
+ * @date 2023/6/16 11:00
+ **/
+public class Test4DoublyCircularLinkedList {
+    /**
+    *
+    * description 测试
+    *
+    *
+    * @return void
+    * @author: woldier
+    * @date: 2023/6/16 11:01
+    */
+    @Test
+    public void test(){
+        DoublyCircularLinkedListSentinel list = new DoublyCircularLinkedListSentinel();
+        Assertions.assertThrows(IllegalArgumentException.class, list::removeFirst);
+        Assertions.assertThrows(IllegalArgumentException.class, list::removeLast);
+        list.addFirst(1);
+        list.addFirst(2);
+        list.addFirst(3);
+        list.addFirst(4);
+        Assertions.assertIterableEquals(Arrays.asList(4,3,2,1),list);
+        list.addLast(0);
+        Assertions.assertIterableEquals(Arrays.asList(4,3,2,1,0),list);
+        list.removeFirst();
+        Assertions.assertIterableEquals(Arrays.asList(3,2,1,0),list);
+        list.removeLast();
+        Assertions.assertIterableEquals(Arrays.asList(3,2,1),list);
+        list.removeByValue(2);
+        Assertions.assertIterableEquals(Arrays.asList(3,1),list);
+
+    }
+}
