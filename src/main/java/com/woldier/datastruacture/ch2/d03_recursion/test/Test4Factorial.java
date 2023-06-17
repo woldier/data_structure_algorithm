652ce4e3a0ffab29ab03875ed124c5890ebbf5d1
root <1098582358@qq.com>
2023-06-17 20:20:39 +0800

    digui ceshi

--- src/main/java/com/woldier/datastruacture/ch2/d03_recursion/test/Test4Factorial.java
+++ src/main/java/com/woldier/datastruacture/ch2/d03_recursion/test/Test4Factorial.java
@@ -4,7 +4,14 @@ import com.woldier.datastruacture.ch2.d03_recursion.Factorial;
 import org.junit.Test;
 
 public class Test4Factorial {
-
+    /**
+    *
+    * description 测试方法注释
+    *
+    * @return
+    * @author: root
+    * @date: 2023/6/17 下午6:28
+    */
     @Test
     public void test() {
         int i = Factorial.f(5);
