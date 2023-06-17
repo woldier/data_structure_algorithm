cdbb2f4397c30a3ea70c06f999a2ccbf03de9a8f
root <1098582358@qq.com>
2023-06-17 10:40:23 +0800

    递归反向打印

---
+++ src/main/java/com/woldier/datastruacture/ch2/d03_recursion/FactorialReversePrintStr.java
@@ -0,0 +1,10 @@
+package com.woldier.datastruacture.ch2.d03_recursion;
+
+
+public class FactorialReversePrintStr {
+    public static void print(String str){
+        System.out.print(str.charAt(str.length() - 1));
+        if (str.length()!=1)
+            print(str.substring(0,str.length()-1));
+    }
+}
---
+++ src/main/java/com/woldier/datastruacture/ch2/d03_recursion/test/TestReversePrint.java
@@ -0,0 +1,11 @@
+package com.woldier.datastruacture.ch2.d03_recursion.test;
+
+import com.woldier.datastruacture.ch2.d03_recursion.FactorialReversePrintStr;
+import org.junit.Test;
+
+public class TestReversePrint {
+    @Test
+    public void print(){
+        FactorialReversePrintStr.print("hello");
+    }
+}
