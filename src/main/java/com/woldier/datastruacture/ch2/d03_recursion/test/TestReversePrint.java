8a27821eedb85f61fe8e1b2999f36582f85dbbb9
woldier <1098582358@qq.com>
2023-06-17 10:57:12 +0800

    反向print 类名修改

--- src/main/java/com/woldier/datastruacture/ch2/d03_recursion/test/TestReversePrint.java
+++ src/main/java/com/woldier/datastruacture/ch2/d03_recursion/test/TestReversePrint.java
@@ -1,11 +1,11 @@
 package com.woldier.datastruacture.ch2.d03_recursion.test;
 
-import com.woldier.datastruacture.ch2.d03_recursion.FactorialReversePrintStr;
+import com.woldier.datastruacture.ch2.d03_recursion.ReversePrintStr;
 import org.junit.Test;
 
 public class TestReversePrint {
     @Test
     public void print(){
-        FactorialReversePrintStr.print("hello");
+        ReversePrintStr.print("hello");
     }
 }
