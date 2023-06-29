3c94e9f9a9fa6ec4b69db50ad70c7bf11c52ac6d
root <1098582358@qq.com>
2023-06-29 12:33:41 +0800

    实现了优先级接口的元素包装类

---
+++ src/main/java/com/woldier/datastruacture/ch2/d04_queue/PriorityElem.java
@@ -0,0 +1,26 @@
+package com.woldier.datastruacture.ch2.d04_queue;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@ToString(callSuper = false)
+@AllArgsConstructor
+@NoArgsConstructor
+public class PriorityElem<E> implements Priority{
+    private E e;
+    private int priority;
+    /**
+     * description 返回优先级的大小,值越大说明优先级越高
+     *
+     * @return 返回优先级的大小, 值越大说明优先级越高
+     * @author: woldier
+     * @date: 2023/6/29 上午10:50
+     */
+    @Override
+    public int priority() {
+        return priority;
+    }
+}
