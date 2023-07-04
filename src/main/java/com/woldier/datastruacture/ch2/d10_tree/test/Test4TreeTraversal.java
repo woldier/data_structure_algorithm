00ed0ff9eb6b6ea4d60a0380feba1019d1a633db
root <1098582358@qq.com>
2023-07-04 17:23:21 +0800

    遍历工具类

---
+++ src/main/java/com/woldier/datastruacture/ch2/d10_tree/test/Test4TreeTraversal.java
@@ -0,0 +1,34 @@
+package com.woldier.datastruacture.ch2.d10_tree.test;
+
+import com.woldier.datastruacture.ch2.d10_tree.TreeNode;
+import com.woldier.datastruacture.ch2.d10_tree.TreeTraversal;
+import org.junit.jupiter.api.Test;
+
+public class Test4TreeTraversal {
+    @Test
+    public void test() {
+
+
+        TreeNode<Integer> p6 = new TreeNode<>(6);
+        TreeNode<Integer> p7 = new TreeNode<>(7);
+        TreeNode<Integer> p3 = new TreeNode<>(p6, 3, p7);
+
+        TreeNode<Integer> p4 = new TreeNode<>(4);
+        TreeNode<Integer> p5 = new TreeNode<>(5);
+        TreeNode<Integer> p2 = new TreeNode<>(p4, 2, p5);
+
+        TreeNode<Integer> p = new TreeNode<>(p2, 1, p3);
+
+
+//        TreeTraversal.preOrder(p,System.out::print);
+//        System.out.println();
+//        TreeTraversal.midOrder(p,System.out::print);
+//        System.out.println();
+//        TreeTraversal.postOrder(p,System.out::print);
+
+//        TreeTraversal.preOrder(p,System.out::print,false);
+//        TreeTraversal.midOrder(p,System.out::print,false);
+        TreeTraversal.postOrder(p,System.out::print,false);
+    }
+
+}
